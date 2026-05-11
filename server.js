5adf6b76d53dda5151816c121508f2275d67bb96
luisfelipe123poi <luiscastellar1505@gmail.com>
2026-05-11 09:50:07 -0500

    server.js

--- server.js
+++ server.js
@@ -1443,6 +1443,11 @@ app.use(express.static(path.join(__dirname, "public"), {
   index: false
 }));
 
+app.use((req, res, next) => {
+  console.log("🔥 REQUEST:", req.method, req.url);
+  next();
+});
+
 app.get("/crm/:slug", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "crm.html"));
 });
